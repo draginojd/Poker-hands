b9fd1968a3e8ac8d895e8873a3cd93b74b120d6b
draginojd <arminfazlikhan@gmail.com>
2023-04-19 22:03:44 +0200

    test not done low battery

--- tests/isStraight.test.js
+++ tests/isStraight.test.js
@@ -1,5 +1,6 @@
 const Hand = require('../Hand');
 const CompareHands = require('../CompareHands');
+const DeckOfCards = require("../DeckOfCards")
 const suits = "♥♦♣♠";
 const ranks = "23456789TJQKA";
 
@@ -20,15 +21,12 @@ test('check that isStraight returns truthy if straight', () => {
 
 test("check that isStraight returns falsy if not straight", () => {
   for (let suit of suits) {
-    for (let rank of 'A23456789T') {
-      let ranksForHand = '';
-      if (rank === 'A') { ranksForHand = 'A2589'; }
-      else {
-        let index = ranks.indexOf(rank);
-        ranksForHand += ranks.slice(index, index + 5);
-      }
-      let hand = new Hand(...[...ranksForHand].map(x => suit + x));
+    for (let rank of "234567") {
+      let hand = new Hand(suit + rank, suit + rank, suit + rank, suit + rank, suit + rank);
+      DeckOfCards.shuffle(hand.cards);
       expect(CompareHands.isStraight(hand)).toBeFalsy();
+      console.log(hand);
     }
   }
-})
+});
+
