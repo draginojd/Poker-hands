9839797e8828131ee01371929cc0ebb597bbea8f
draginojd <arminfazlikhan@gmail.com>
2023-04-19 20:47:09 +0200

    check that isStraight returns truthy

--- tests/isStraight.test.js
+++ tests/isStraight.test.js
@@ -4,14 +4,31 @@ const suits = "♥♦♣♠";
 const ranks = "23456789TJQKA";
 
 test('check that isStraight returns truthy if straight', () => {
-  for (let rank of ranks) {
-    let hand = new Hand('♥' + rank, '♦' + rank, '♣' + rank, '♥' + rank, '♦' + rank);
-    expect(CompareHands.isStraight(hand)).toBeTruthy();
+  for (let suit of suits) {
+    for (let rank of 'A23456789T') {
+      let ranksForHand = '';
+      if (rank === 'A') { ranksForHand = 'A2345'; }
+      else {
+        let index = ranks.indexOf(rank);
+        ranksForHand += ranks.slice(index, index + 5);
+      }
+      let hand = new Hand(...[...ranksForHand].map(x => suit + x));
+      expect(CompareHands.isStraight(hand)).toBeTruthy();
+    }
   }
 });
 
 test("check that isStraight returns falsy if not straight", () => {
-  for (let rank of ranks) {
-    let hand = new Hand("")
+  for (let suit of suits) {
+    for (let rank of 'A23456789T') {
+      let ranksForHand = '';
+      if (rank === 'A') { ranksForHand = 'A2589'; }
+      else {
+        let index = ranks.indexOf(rank);
+        ranksForHand += ranks.slice(index, index + 5);
+      }
+      let hand = new Hand(...[...ranksForHand].map(x => suit + x));
+      expect(CompareHands.isStraight(hand)).toBeFalsy();
+    }
   }
 })
