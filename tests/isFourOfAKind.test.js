c6d2615321cf0b92cd280e95cdeb14fde868edc5
draginojd <arminfazlikhan@gmail.com>
2023-04-19 15:04:36 +0200

    added is straight? foruofakind syntax correction

--- tests/isFourOfAKind.test.js
+++ tests/isFourOfAKind.test.js
@@ -5,10 +5,13 @@ const ranks = "23456789TJQKA";
 
 test("Check that isFourOfAKind returns truthy2", () => {
     let prevRank;
-    for (let rank of ranks) {
-        let hand = new Hand("♣" + rank, "♥" + rank, "♦" + rank, "♠" + rank, "♥" + (prevRank ? prevRank : "A"));
-        prevRank = rank;
-        expect(CompareHands.isFourOfAKind(hand)).toBeTruthy();
+    let prevSuit;
+    for (let suit of suits) {
+        for (let rank of ranks) {
+            let hand = new Hand("♣" + rank, "♥" + rank, "♦" + rank, "♠" + rank, "♥" + (prevRank ? prevRank : "A"));
+            prevRank = rank;
+            expect(CompareHands.isFourOfAKind(hand)).toBeTruthy();
+        }
     }
 });
 
@@ -31,18 +34,17 @@ test("Check that isFourOfAKind returns falsy2", () => {
     }
 });
 
-test('Check that isFourOfAKind returns a higher score for the stronger hand (if two hands but with four of a kind', () => {
-    
-        let hand1 = new Hand( "♥9", "♦9", "♣9", "♠9", "♠2");
-        let hand2 = new Hand("♣4", "♠T", "♠T", "♠T", "♠T");
+test('Check that isFourOfAKind returns a higher score(toBeGreaterThan) for the stronger hand (if two hands but with four of a kind', () => {
 
-        let hand1Score = CompareHands.isFourOfAKind(hand1);
-        let hand2Score = CompareHands.isFourOfAKind(hand2);
-        expect(hand2Score).toBeGreaterThan(hand1Score);
+    let hand1 = new Hand("♥9", "♦9", "♣9", "♠9", "♠2");
+    let hand2 = new Hand("♣4", "♠T", "♠T", "♠T", "♠T");
+
+    let hand1Score = CompareHands.isFourOfAKind(hand1);
+    let hand2Score = CompareHands.isFourOfAKind(hand2);
+    expect(hand2Score).toBeGreaterThan(hand1Score);
 });
 
-test('Check that isFourOfAKind returns a higher score for the stronger hand (if two hands but with four of a kind', () => {
-
+test('Check that isFourOfAKind returns lower score(toBeLessThan) for the weakest hand (if two hands but with four of a kind', () => {
     let hand1 = new Hand("♥9", "♦9", "♣9", "♠9", "♠5");
     let hand2 = new Hand("♣4", "♠9", "♠9", "♠9", "♠9");
 
--- tests/isStraight.test.js
+++ tests/isStraight.test.js
@@ -1,8 +1,17 @@
 const Hand = require('../Hand');
 const CompareHands = require('../CompareHands');
-const suits = '♥♦♣♠';
+const suits = "♥♦♣♠";
+const ranks = "23456789TJQKA";
 
 test('check that isStraight returns truthy if straight', () => {
-  let hand = new Hand('♥9', '♦8', '♣7', '♥5', '♦6');
-  expect(CompareHands.isStraight(hand)).toBeTruthy();
+  for (let rank of ranks) {
+    let hand = new Hand('♥' + rank, '♦' + rank, '♣' + rank, '♥' + rank, '♦' + rank);
+    expect(CompareHands.isStraight(hand)).toBeTruthy();
+  }
 });
+
+test("check that isStraight returns falsy if not straight", () => {
+  for (let rank of ranks) {
+    let hand = new Hand("")
+  }
+})
