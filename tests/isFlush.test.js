0be6cc520c9f70c9c754640c76cecbebe847be19
draginojd <arminfazlikhan@gmail.com>
2023-04-20 12:47:08 +0200

    isFlush toBeLessThan comparer

--- tests/isFlush.test.js
+++ tests/isFlush.test.js
@@ -21,3 +21,11 @@ test('check that isFlush returns a higher score for a stronger hand (if two hand
   let hand2Score = CompareHands.isFlush(hand2);
   expect(hand2Score).toBeGreaterThan(hand1Score);
 });
+
+test('check that isFlush returns a higher score for a stronger hand (if two hands but with flush)', () => {
+  let hand1 = new Hand('♣2', '♣6', '♣4', '♣8', '♣7');
+  let hand2 = new Hand('♦T', '♦9', '♦5', '♦Q', '♦A');
+  let hand1Score = CompareHands.isFlush(hand1);
+  let hand2Score = CompareHands.isFlush(hand2);
+  expect(hand1Score).toBeLessThan(hand2Score);
+});
